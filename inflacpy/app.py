b4386e47ff111c6837ec0db509662d304dc74ea2
Felipe Menino <efelipecarlos@gmail.com>
2018-03-04 00:01:32 -0300

    Atualização da forma do export, para a correção de erro com campos nulos

--- inflacpy/app.py
+++ inflacpy/app.py
@@ -4,7 +4,8 @@ from scrap.scrap import Scrap
 
 if __name__ == '__main__':
     scrap = Scrap()
-    _list = scrap.get_data(country='brasil')
+    _list = scrap.get_data(country='brasil', year_start=2016)
 
     # Exportando em json
-    pd.DataFrame(_list).to_json('../data/brasil.json')
+    _json = pd.DataFrame(_list).dropna()
+    _json.to_json('../data/brasil.json')
